05fda6f1c672d9c016b4991ed03e4d2e99a5cb12
Aaron Bos <aaronmbos@gmail.com>
2022-02-16 20:14:53 -0600

    Initial commit

---
+++ dev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#brew install git
+
+#brew install --cask kitty
+
+brew install neovim
+
+#brew install --cask firefox
+
+#brew install --cask sublime-text
+
+#brew install --cask azure-data-studio
+
+# Oh My Zsh
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Node Version Manager
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+#brew install --cask dotnet-sdk
